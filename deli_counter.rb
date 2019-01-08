eda809f5f63c7daf4f1b16dd919de93da01c11d6
eselbeus <eselbeus@gmail.com>
2019-01-08 22:56:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,11 @@ katz_deli = []
 
 def line(katz_deli)
   if katz_deli.length == 0 
-    return "The line is currently empty."
+    puts "The line is currently empty."
   end
-  
+  line = "The line is currently:"
+  katz_deli.each_with_index do |person, idx|
+    line += " #{idx + 1}. #{person}"
+  end
+  puts line
 end
